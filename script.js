a15c84e7d246fd82c33417962bb7a1a99976f50d
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-21 15:38:17 +0330

    func:like post by double click on image

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // like post
 const likeIcon = document.getElementById("like");
 const postImage = document.getElementById("post-image");
+const postLike = document.getElementById("like-heart");
 
 // like by click on like icon
 likeIcon.addEventListener("click", handleLike);
@@ -8,4 +9,16 @@ function handleLike() {
   if (likeIcon.src.match("./assets/images/like-icon.svg")) {
     likeIcon.src = "./assets/images/liked-icon.svg";
   } else likeIcon.src = "./assets/images/like-icon.svg";
+}
+
+// like by double click on image
+postImage.addEventListener("dblclick", handleLikeByDblClick);
+function handleLikeByDblClick() {
+  if (likeIcon.src.match("./assets/images/like-icon.svg")) {
+    likeIcon.src = "./assets/images/liked-icon.svg";
+    postLike.classList.add("active");
+    setTimeout(() => {
+      postLike.classList.remove("active");
+    }, 300);
+  }
 }
